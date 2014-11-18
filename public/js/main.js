fbb95f5e0a560c8695eb4ff789d43d37637a409b
Donald Derek <donaldderek.haddad@gmail.com>
2014-11-18 14:13:24 -0500

    online

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    
-    var online = false;
+
+    var online = true;
     var client_id = "";
-    
+
     function msToTime(s) {
         var ms = s % 1000;
         s = (s - ms) / 1000;
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
         return hrs + '.' + mins + '.' + secs;
     }
-    
+
     if(!online){
         client_id = "18057c47126031a99017661840257417";
         SC.initialize({
@@ -25,17 +25,17 @@ $(document).ready(function(){
         client_id = "988ae8ccee6597e1d4e45ac225d80ea2";
         SC.initialize({
             client_id: client_id,
-            redirect_uri: "http://preso.ly:3000/callback"
+            redirect_uri: "http://donald.ws:3000/callback"
         });
     }
-    
+
     //TODO:Filter By Score
     $("#score").on('click',function(e){
         $(".tracks").each( function(){});
     });
-  
-    
-    
+
+
+
     //Filter dropdown sort-nav
     $(".filter").on('click',function(e){
         if($(this).attr("id") == "tracks"){
@@ -51,32 +51,32 @@ $(document).ready(function(){
         $(".s-block").hide();
         $("."+$(this).attr('id')).fadeIn(300);
     });
-    
+
     //Trigger modal and listen to track with comments visuals
     $('.listen').on('click', '.tracks', function(){
-        
+
         var track_id = $(this).data("id");
         var score = $(this).data("score");
         var artwork_url = $(this).find('img').attr('src');
         var title = $(this).find('.title').text();
-        
+
         var downloadable = $(this).find('#download-url').data('d');
         var download_url = $(this).find('#download-url').data('url');
         var track_duration = msToTime($(this).data("duration"));
-        
+
         //Trigger Loader
         $(".loader-gif-right").fadeIn('fast');
         $(".loader-gif-left").fadeIn('fast');
-        
+
         $(".player").append("<div class='info-block'><div>");
         $(".info-block").append("<img src='" + artwork_url + "' />");
         $(".info-block").append("<h2>" + title + "</h2>");
         $(".info-block").append("<p> N!MP Score: " + score + "</p>");
-        
-        
+
+
         $(".info-block").append("<a class='download-track' href='"+download_url+"?client_id="+client_id+"' target='_BLANK'>.mp3 <i class=' icon-arrow-down icon-white'><i/></a>")
-        
-        
+
+
         $(".l-comments").addClass('isLeft');
         SC.get("/tracks/" + track_id, function(track){
             $.getJSON("http://waveformjs.org/w?callback=?", {
@@ -102,22 +102,22 @@ $(document).ready(function(){
                 //var streamOptions = waveform.optionsForSyncedStream();
                 var artist_name = track.user.username;
                 $(".artist-name").text(artist_name);
-                SC.stream(track.uri, 
+                SC.stream(track.uri,
                 //Visualise Comments
-                {   
+                {
                     ontimedcomments: function(comments){
-                
+
                         //Middle Comments Area
                         $(".m-comments").show();
-                
+
                         //Left Comments Area
                         $(".l-comments").show();
-                
+
                         //Right Comments Area
                         $(".r-comments").show();
-                
+
                         var comment = "<div class='comment'>" + comments[0].body + "</div>";
-                
+
                         //Insert Comment in appropriate Area
                         if($(".l-comments").hasClass('isLeft') && $(comment).text().length < 32 ){
                             $(".l-comments").prepend($(comment).hide()
@@ -135,36 +135,36 @@ $(document).ready(function(){
                             $(".l-comments").addClass('isLeft');
                             $(".r-comments").removeClass('isRight');
                         }
-                        else{    
+                        else{
                             $(".m-comments").html($(comment).hide()
                                 .fadeIn("fast")
                                 .delay(10000)
                                 .fadeOut('fast'));
                         }
-                        
+
                     }
-                    
+
                 },
-                
-                
-        
+
+
+
                 //Play Track
                 function(sound){
-            
+
                     sound.play({
                         onfinish: function() {
                             $("#player").modal('hide');
                         },
                         whileloading: function(){
-                            $("#duration").html("<span class='track-duration'>" + msToTime(this.position) + "</span> / <span class='track-duration'>" + track_duration + "</span>");    
-                            
+                            $("#duration").html("<span class='track-duration'>" + msToTime(this.position) + "</span> / <span class='track-duration'>" + track_duration + "</span>");
+
                         },
                         onplay: function(){
                             $(".loader-gif-right").fadeOut('fast');
                             $(".loader-gif-left").fadeOut('fast');
                         }
                     });
-                    
+
                     //Pause/ Play
                     $(".plpo").on('click',function(e){
                         console.log($(this).hasClass('play'));
@@ -181,71 +181,71 @@ $(document).ready(function(){
                             $(this).find('span').text('pause');
                         }
                     });
-            
+
                     //When Modal is hidden
                     $('#player').on('hidden', function () {
-                
-                        //Reset Play/Pause 
+
+                        //Reset Play/Pause
                         $(".plpo").off('click');
                         $(".plpo").removeClass('pause');
                         $(".plpo").addClass('play');
                         $(".plpo").find('span').text('pause');
-                
+
                         //Hide Modal and remove comments
                         $("#player").modal('hide');
                         $(".info-block").remove();
                         $(".comment").remove();
                         $(".comments").hide();
-                        
+
                         //reset waveform
                         $("#waveform").html("");
                         $(".artist-name").html("");
                         sound.stop();
-    
-                    });    
+
+                    });
                 });
             });
         });
         $('#player').modal('show');
-        
+
         //When Model is close with the x button
         $(".close").on('click',function(){
             $("#player").modal('hide');
         });
-    
+
     });
-    
+
     //Trigger Modal with artist page
     $('.listen').on('click', '.artists', function(){
-        
+
         });
 
-    //Get Artists and Tracks upon Login    
+    //Get Artists and Tracks upon Login
     $("#connect").one('click',function(e){
-        
+
         //Sound Cloud Connection
         SC.connect(function(){
-            
+
             //GET user info
             SC.get("/me", function(me){
-                
+
                 $("#username").text(me.username);
                 $("#avatar img").attr("src",me.avatar_url);
                 $("#connect").fadeOut(200);
                 $('.logged-in').delay(200).fadeIn(300);
-                
+
                 //GET User's Following
                 SC.get("/me/followings", function(followings){
-                    
-                    var tracks_number = 0;    
-                    
+
+                    var tracks_number = 0;
+
                     $(followings).each(function(i, following){
-                        
+
                         if(following.avatar_url && following.track_count > 0){
-                            
+
                             //Calculate Reputation Score
                             var reputation = following.public_favorites_count + following.followers_count*0.3;
-                            
+
                             //Generate Artist Block
                             var block = "<div data-id='"+following.id+"' class='s-block artists'>\n\
                                                 <img src='"+following.avatar_url+"'/>\n\
@@ -257,21 +257,21 @@ $(document).ready(function(){
                                             </div>";
                             $('.l-holder').append(block);
                         }
-                        
+
                         //GET User's Tracks
                         SC.get("/users/" + following.id + "/tracks", function(tracks){
                             tracks_number += tracks.length;
-                            
+
                             var single_track = {};
-                            
+
                             $('.track').text(tracks_number + " tracks");
-                            
-                            
+
+
                             $(tracks).each(function(j, track){
-                                
+
                                 //Calculate Buzz
                                 var buzz = 0;
-                                
+
                                 if(track.commentable){
                                     buzz += track.comment_count*2;
                                 }
@@ -281,10 +281,10 @@ $(document).ready(function(){
                                 buzz += track.playback_count*0.3;
                                 buzz += track.favoritings_count*0.7;
                                 buzz = parseInt(buzz);
-                                
-                                
 
-                                
+
+
+
                                 //Generate Track Block
                                 if(track.artwork_url){
                                     single_track = {
@@ -305,18 +305,17 @@ $(document).ready(function(){
                                                     <h2 class='title'>"+track.title+"</h2>\n\
                                                     <p>N!MP Score: "+ parseInt(buzz) +"</p>\n\
                                                 </div>";
-                                    
+
                                     block += "<div data-d='"+track.downloadable+"' data-url='"+track.download_url+"' id='download-url'></div>";
-                                    
+
                                     block += "</div>";
                                     $('.l-holder').append(block);
-                                }     
-                            }); 
-                        }); 
-                    });            
+                                }
+                            });
+                        });
+                    });
                 });
             });
-        }); 
+        });
     });
 });
-
